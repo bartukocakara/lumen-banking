76903943706ec4e7e3e231c18686eadb4580616c
bartukocakara <kocakarabartu@gmail.com>
2022-07-25 15:15:41 +0300

    Docker compose multiple db

--- src/banking/app/Repositories/Concrete/EntryRepository.php
+++ src/banking/app/Repositories/Concrete/EntryRepository.php
@@ -6,10 +6,12 @@ use Illuminate\Support\Facades\DB;
 use App\Traits\ApiResponse;
 use App\Repositories\Abstracts\IEntryRepository;
 use App\Traits\ApiResponseTrait;
+use Illuminate\Support\Facades\Artisan;
 
 class EntryRepository extends BaseRepository implements IEntryRepository
 {
     use ApiResponseTrait;
+    protected $connection = 'remote_mysql';
 
     public function synchronisation()
     {
--- src/banking/config/database.php
+++ src/banking/config/database.php
@@ -41,13 +41,29 @@ return [
             'prefix' => env('DB_PREFIX', ''),
         ],
 
-        'mysql' => [
+        'local_mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', 3306),
+            'port' => env('DB_PORT', 3310),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => env('DB_CHARSET', 'utf8mb4'),
+            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => env('DB_PREFIX', ''),
+            'strict' => env('DB_STRICT_MODE', false),
+            'engine' => env('DB_ENGINE', null),
+            'timezone' => env('DB_TIMEZONE', '+00:00'),
+        ],
+
+        'remote_mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_REMOTE_HOST', '127.0.0.1'),
+            'port' => env('DB_REMOTE_PORT', 3306),
+            'database' => env('DB_REMOTE_DATABASE', 'forge'),
+            'username' => env('DB_REMOTE_USERNAME', 'forge'),
+            'password' => env('DB_REMOTE_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
